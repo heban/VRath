905c30175e0e91616dfba1f8610bf7a4fb601667
heban <pawel.ochota@gmail.com>
2017-05-17 18:53:05 +0200

    Final target

--- src/assets/js/components/camera.component.jsx
+++ src/assets/js/components/camera.component.jsx
@@ -2,11 +2,11 @@ import { Entity } from "aframe-react";
 import React from "react";
 
 const CameraComponent = () => (
-    <Entity primitive="a-camera" wasd-controls="enabled: true" position="0 0 10">
+    <Entity primitive="a-camera" wasd-controls="enabled: false" position="0 0 10">
         <Entity primitive="a-cursor" 
             geometry="primitive: ring; radiusInner: 0.00001; radiusOuter: 0.04" 
             material="src: #crosshairTexture" 
-            sound="src: #shot; on: click; poolSize: 10" />
+            sound="src: #shot; on: click; poolSize: 10; volume: 0.2" />
     </Entity>
 );
 
